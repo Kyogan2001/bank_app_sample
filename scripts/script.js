ddc1cbd00302372f0c1e20b8d207cf90957fbdc7
Kyogan2001 <83542145+Kyogan2001@users.noreply.github.com>
2021-06-20 19:26:14 +0530

    adding user function done

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,46 @@
-'use script'
+'use script';
+
+const main = document.getElementById('main');
+const btnAddUser = document.getElementById('add-user');
+const btnDouble = document.getElementById('double');
+const btnFilterRich = document.getElementById('filter-rich');
+const btnTotal = document.getElementById('total');
+
+//adduser btn
+let data = [];
+
+//fetch a random user using random user api
+const getRandomUser = async function(){
+    const response = await fetch('https://randomuser.me/api');
+    const data = await response.json();
+    const user = data.results[0];
+
+    const newUser = {
+        name:`${user.name.first} ${user.name.last}`,
+        balance:Math.floor(Math.random()*100000)
+    };
+    addData(newUser);
+}
+
+const addData = function(obj){
+    data.push(obj);
+    updateDom();
+};
+
+const updateDom = function(provideData = data) {
+    //clear main
+    main.innerHTML = '<h2><strong>Name</strong> Balance</h2>';
+    provideData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('users');
+        element.innerHTML = `<strong>${item.name}</strong>${item.balance}`;
+        main.appendChild(element);
+    })
+}
+
+
+
+
+btnAddUser.addEventListener('click',getRandomUser);
+btnDouble.addEventListener('click',balancedouble);
+//update dom with random user
